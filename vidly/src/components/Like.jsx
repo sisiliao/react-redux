ab4c0d7fabfe05aa3df289ed0b42b9a10c6ea76e
sss1217 <siyun.liao@mail.mcgill.ca>
2021-02-10 22:20:54 -0500

    Change <Like> to functional component.

--- vidly/src/components/Like.jsx
+++ vidly/src/components/Like.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 //Input: liked: boolean
 //Output: onClick
 
-export default class Like extends Component {
-  render() {
-    console.log(this.props)
-    return (
-      <div onClick={this.props.onLike}>
-        {this.props.liked ? (
-          <i className='fas fa-heart'></i>
-        ) : (
-          <i className='far fa-heart'></i>
-        )}
-      </div>
-    )
-  }
+const Like = (props) => {
+  return (
+    <div onClick={props.onLike}>
+      {props.liked ? (
+        <i className='fas fa-heart'></i>
+      ) : (
+        <i className='far fa-heart'></i>
+      )}
+    </div>
+  )
 }
+
+export default Like
